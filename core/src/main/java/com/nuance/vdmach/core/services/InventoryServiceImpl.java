38b8dbb29e6622139473e109d7c7d01668bcfdce
edi <edi@darkice.com>
2015-08-23 20:46:05 -0400

    Improve logging

--- core/src/main/java/com/nuance/vdmach/core/services/InventoryServiceImpl.java
+++ core/src/main/java/com/nuance/vdmach/core/services/InventoryServiceImpl.java
@@ -8,6 +8,9 @@ import javax.inject.Named;
 import com.nuance.vdmach.common.vo.ItemDTO;
 import com.nuance.vdmach.model.repositories.ItemRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author ediband1
@@ -16,16 +19,22 @@ import com.nuance.vdmach.model.repositories.ItemRepository;
 @Named
 public class InventoryServiceImpl implements InventoryService {
 
+    private static final Logger logger = LoggerFactory.getLogger(InventoryServiceImpl.class);
+
     @Inject
     ItemRepository itemRepository;
 
     @Override
     public List<ItemDTO> findAllItems() {
+        logger.info("Fetching all items in inventory");
+
         return itemRepository.findAllItems();
     }
 
     @Override
     public void decreaseItemCountInInventory(Long itemId, Integer count) {
+        logger.info("Decreasing item count of item with id [" + itemId + "] by [" + count + "]");
+
         itemRepository.decreaseItemCount(itemId, count);
     }
 }
